cbb611dc47afabeacb7c3b84241f4b2f66b39e74
Farhan Khan <fk27179@gmail.com>
2025-06-09 14:20:38 -0400

    updated homepage

--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,19 @@ from flask import render_template
 @app.route('/index')
 
 def index():
+    #placeholder user
     user = {
         "username": "Bob"
     }
-    return render_template("index.html", title="Home", user=user)
+    #placeholder database
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': "John's post here."
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': "Susan's post here."
+        },
+    ]
+    return render_template("index.html", title="Home", user=user, posts=posts)
