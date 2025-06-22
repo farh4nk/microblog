432410385c967a0ade99510a4c001a41ea45da5b
Farhan Khan <fk27179@gmail.com>
2025-06-21 22:54:43 -0400

    Added user registration feature

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,35 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+import sqlalchemy as sa 
+from app import db
+from app.models import User
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+    
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Enter Password Again', validators=[DataRequired(), EqualTo(password)])
+    submit = SubmitField('Register')
+    
+    def validate_username(self, username):
+        user = db.session.scalar(sa.select(User).where(
+            User.username == username.data
+        ))
+        if user is not None:
+            raise ValidationError('This username is already taken.')
+        
+    def validate_email(self, email):
+        user = db.session.scalar(sa.select(User).where(
+            User.username == email.data
+        ))
+        if user is not None:
+            raise ValidationError('This email address is already registered to an account.')
     
     
